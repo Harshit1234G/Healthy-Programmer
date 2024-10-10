6994c6da54d5bb588d98a091d80513224f1ee07f
Harshit <harshitkumawat849@gmail.com>
2024-10-10 20:51:27 +0530

    Added different types of breaks and messages

--- healthy_programmer.py
+++ healthy_programmer.py
@@ -1,18 +1,36 @@
 from time import sleep
 sleep(30)
 
-from datetime import datetime
-import pyttsx3
 from win10toast import ToastNotifier
+import pyttsx3
 
-engine = pyttsx3.init('sapi5')
-engine.setProperty('rate', 150)
 
-notifier = ToastNotifier()
+def notify_and_say(message: str, minutes: int) -> None:
+    print(f'{minutes} minutes had passed, {message}')
 
-while True:
-    sleep(15*60)
-    notifier.show_toast("Healthy Programmer", "Take a Break!")
-    print("Take Break.", datetime.now())
-    engine.say("Take a break. " * 5)
+    engine = pyttsx3.init('sapi5')
+    engine.setProperty('rate', 150)
+
+    notifier = ToastNotifier()
+    notifier.show_toast("Healthy Programmer", message)
+
+    engine.say(message * 5)
     engine.runAndWait()
+
+
+if __name__ == '__main__':
+    time: int = 0
+
+    while True:
+        sleep(300)
+        time += 5
+
+        if time % 10 == 0:
+            notify_and_say('Rest your eyes. ', time)
+
+        if time % 25 == 0:
+            notify_and_say('Drink Water. ', time)
+
+        if time % 60 == 0:
+            notify_and_say(
+                'Do some physical activity and maintain posture. ', time)
